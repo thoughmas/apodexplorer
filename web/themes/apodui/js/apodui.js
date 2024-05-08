5b8fcbe59d2b3f45823552d0e6817f106c5fa4db
Thomas Newman <thoughmas@icloud.com>
2024-05-08 12:16:23 -0700

    create custom theme

---
+++ web/themes/apodui/js/apodui.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * APOD Theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.apodui = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
